b8b790751c8bcca00fb49cd687921d07c32ad6a6
sunil <sma@codebee.dk>
2019-04-12 11:00:34 +0545

    Complete CRUD Operation

--- src/app/dashboard.service.ts
+++ src/app/dashboard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient,HttpHeaders} from '@angular/common/http';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { ProductModule } from './product/product.module';
 
@@ -8,23 +8,34 @@ import { ProductModule } from './product/product.module';
 })
 export class DashboardService {
   
+   httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json'
+    })
+    };
+
 
   constructor(private http:HttpClient) { }
   baseUrl:string="http://localhost:62215/api/product/";
 
+  
   getAllProduct()
     {
         return this.http.get<ProductModule[]>(this.baseUrl+"GetAllProductDetails");
     }
 
-    insertProduct(){
-
+    insertProduct(productName:ProductModule){
+      return this.http.post(this.baseUrl+"InsertProductDetails",JSON.stringify(productName),this.httpOptions);
     }
 
-    updateProduct(){
+    getProductById(id:number){
+      return this.http.get<ProductModule[]>(this.baseUrl+"GetProductById/"+id);
+    }
 
+    updateProduct(productName:ProductModule){
+      return this.http.put(this.baseUrl+"UpdateProductDetails",JSON.stringify(productName),this.httpOptions);
     }
     deleteProduct(id:number){
-      return this.http.delete(this.baseUrl+"DeleteProduct");
+      return this.http.delete(this.baseUrl+"DeleteProduct/"+id);
     }
 }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -22,23 +22,63 @@ export class DashboardComponent implements OnInit {
   lists:ProductModule[];
   displayedColumns: string[] = ['id', 'product','deleted','action'];
 
+  productForm=new FormGroup({
+    productId:new FormControl(),
+      productName: new FormControl('',[
+        Validators.required
+      ]),
+      deleted:new FormControl()
+  });
+
   getAllProduct(){
    this.service.getAllProduct().subscribe
     (data => this.lists = data);
   }
 
-  insertProduct(productName:string){
-    alert(productName);
+  submitProduct(productName){
+    if(productName.productId>0){
+      this.updateProduct(productName)
+    }
+    else{
+      this.insertProduct(productName);
+    }
+    
   }
 
-  updateProduct(id:number){
-    alert(id);
+  insertProduct(productName){
+    this.service.insertProduct(productName).subscribe(
+      data =>{
+        this.getAllProduct(),
+        this.resetForm()
+      }
+    );
+  }
+
+  updateProductById(id:number){
+    this.service.getProductById(id).subscribe(
+      data =>{
+        this.productForm.setValue({productId:data.ProductId,productName:data.ProductName,deleted:data.Deleted})
+      }
+    );
+  }
+
+  updateProduct(productName){
+    this.service.updateProduct(productName).subscribe(
+      data =>{
+        this.getAllProduct(),
+        this.resetForm()
+      }
+    );
   }
 
   deleteProduct(id:number){
-    this.service.deleteProduct(id).subscribe(
-      this.ngOnInit
-    );
+      this.service.deleteProduct(id).subscribe(
+        data =>{this.getAllProduct()}
+      );
+  }
+
+  resetForm(){
+    this.productForm.reset();
   }
   
 } 
